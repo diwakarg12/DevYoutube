1d459286eae292b11b4a1a0e39a1daa0f768c16b
Diwakar Giri <85389557+diwakarg12@users.noreply.github.com>
2023-12-11 11:20:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  var API_KEY = "AIzaSyDP-a9H6wVD0xs6kBPi3NJ_bBXevujnUjc";
+
+  var video = "";
+  //   var videos = $("#videos")
+
+  $("#form").submit(function (event) {
+    event.preventDefault();
+
+    var search = $("#search").val();
+
+    videoSearch(API_KEY, search, 10);
+  });
+
+  function videoSearch(key, search, maxResults) {
+    $("#videos").empty();
+
+    $.get(
+      "https://www.googleapis.com/youtube/v3/search?key=" +
+        key +
+        "&type=video&part=snippet&maxResults=" +
+        maxResults +
+        "&q=" +
+        search,
+      function (data) {
+        console.log(data);
+
+        data.items.forEach((item) => {
+          video = `
+            <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+            `;
+
+          $("#videos").append(video);
+        });
+      }
+    );
+  }
+});
